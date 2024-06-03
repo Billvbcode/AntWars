e2eb26e0b649360e4803010f99c80d30567ca59b
William Perkett <perkettbill@gmail.com>
2024-06-03 18:54:57 -0400

    Add files via upload

---
+++ 23-AntWarsCS/AntWars/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace AntWars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TimMove = new System.Windows.Forms.Timer(this.components);
+            this.L1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.T1 = new System.Windows.Forms.Timer(this.components);
+            this.A1 = new System.Windows.Forms.PictureBox();
+            this.A2 = new System.Windows.Forms.PictureBox();
+            this.A3 = new System.Windows.Forms.PictureBox();
+            this.A4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A4)).BeginInit();
+            this.SuspendLayout();
+           
+            // 
+            // L1
+            // 
+            this.L1.AutoSize = true;
+            this.L1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L1.Location = new System.Drawing.Point(564, 370);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(57, 20);
+            this.L1.TabIndex = 7;
+            this.L1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(562, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 44);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // T1
+            // 
+            this.T1.Interval = 200;
+            this.T1.Tick += new System.EventHandler(this.T1_Tick);
+            // 
+            // A1
+            // 
+            this.A1.Image = ((System.Drawing.Image)(resources.GetObject("A1.Image")));
+            this.A1.Location = new System.Drawing.Point(88, 224);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(71, 74);
+            this.A1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A1.TabIndex = 9;
+            this.A1.TabStop = false;
+            this.A1.Click += new System.EventHandler(this.A1_Click);
+            // 
+            // A2
+            // 
+            this.A2.Image = ((System.Drawing.Image)(resources.GetObject("A2.Image")));
+            this.A2.Location = new System.Drawing.Point(225, 224);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(71, 74);
+            this.A2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A2.TabIndex = 10;
+            this.A2.TabStop = false;
+            this.A2.Click += new System.EventHandler(this.A2_Click);
+            // 
+            // A3
+            // 
+            this.A3.Image = ((System.Drawing.Image)(resources.GetObject("A3.Image")));
+            this.A3.Location = new System.Drawing.Point(365, 216);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(71, 74);
+            this.A3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A3.TabIndex = 11;
+            this.A3.TabStop = false;
+            this.A3.Click += new System.EventHandler(this.A3_Click);
+            // 
+            // A4
+            // 
+            this.A4.Image = ((System.Drawing.Image)(resources.GetObject("A4.Image")));
+            this.A4.Location = new System.Drawing.Point(509, 216);
+            this.A4.Name = "A4";
+            this.A4.Size = new System.Drawing.Size(71, 74);
+            this.A4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A4.TabIndex = 12;
+            this.A4.TabStop = false;
+            this.A4.Click += new System.EventHandler(this.A4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 506);
+            this.Controls.Add(this.A4);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.L1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer TimMove;
+        private System.Windows.Forms.Label L1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer T1;
+        private System.Windows.Forms.PictureBox A1;
+        private System.Windows.Forms.PictureBox A2;
+        private System.Windows.Forms.PictureBox A3;
+        private System.Windows.Forms.PictureBox A4;
+    }
+}
+
---
+++ 23-AntWarsCS/AntWars/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntWars
+{
+    public partial class Form1 : Form
+    {
+        int iScore; // To Keep Score
+        int iTick;  // To Speed up Timer
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+   
+
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            L1.Text = "Hi";
+            T1.Start(); //Start Timer
+            A1.Top = 400; //Reset Ants
+            A2.Top = 400;
+            A3.Top = 400;
+            A4.Top = 400;
+            A4.Visible = false;
+            iScore = 0;  //Reset Score
+            L1.Text = "Score= " + iScore;
+        }
+
+        private void T1_Tick(object sender, EventArgs e)
+        {
+            A1.Top -= 10; //Move Ant
+            A2.Top -= 10; //Move Ant2
+            A3.Top -= 10; //Move Ant3
+            if (A4.Visible) //Only move Ant4 When Visible
+                A4.Top -= 10; //Move Ant4 when visible
+            if (iScore > 10) //At a score of 10
+                A4.Visible = true; //Make Ant4 visible
+
+            if (A1.Top<0 | A2.Top < 0) //A1 Off Screen
+            {
+                T1.Stop(); // Stop Timer
+                L1.Text += "  You Lost!";
+            }
+            T1.Interval = 200 - iScore;  //Speed Up
+            this.Text= "Speed=" + T1.Interval;  //Show Speed
+        }
+
+        private void A1_Click(object sender, EventArgs e)
+        {
+            if (T1.Enabled == false)
+                return; //Exit if Timer not running
+            A1.Top = 400;
+            iScore += 5;  //Add to Score
+            L1.Text = "Score= " + iScore;
+        }
+
+        private void A2_Click(object sender, EventArgs e)
+        {
+            A2.Top = 400;
+            iScore += 5;  //Add to Score
+            L1.Text = "Score= " + iScore;
+        }
+
+        private void A3_Click(object sender, EventArgs e)
+        {
+            A3.Top = 400;
+            iScore += 5;  //Add to Score
+            L1.Text = "Score= " + iScore;
+        }
+
+        private void A4_Click(object sender, EventArgs e)
+        {
+            A4.Top = 400;
+            iScore += 5;  //Add to Score
+            L1.Text = "Score= " + iScore; 
+        }
+    }
+}
